96206d75b75d390819ed2a7bb6bb2b5be3f46062
aryabh <aryabhthakur@yahoo.com>
2022-03-02 09:16:43 +0530

    responsive issue fixed

--- components/Layouts/GuestLayout.js
+++ components/Layouts/GuestLayout.js
@@ -16,7 +16,7 @@ const GuestLayout = ({ children }) => {
                 <title>{process.env.NEXT_PUBLIC_APPLICATION_NAME} site</title>
             </Head>
             <div className="container mx-auto dark:bg-slate-900">
-                <div className="flex sticky top-5 shadow-lg dark:shadow-2xl dark:shadow-slate-700 p-4 bg-white dark:bg-slate-800 rounded-lg border">
+                <div className="flex sticky top-0 md:top-5 shadow-lg dark:shadow-2xl dark:shadow-slate-700 p-4 bg-white dark:bg-slate-800 z-50 md:rounded-lg border">
                     <div className="mr-auto">
                         <Link href="/">
                             <button  className='mr-2 py-1 px-2 shadow dark:hover:shadow hover:shadow-lg hover:shadow-orange-200 hover:bg-orange-300 dark:hover:text-slate-900 dark:active:text-white active:bg-orange-500 active:text-white duration-200 hover:rounded hover:border-transparent dark:border-blue-500 dark:hover:border-transparent border-b-2'>About Me</button>
@@ -25,23 +25,23 @@ const GuestLayout = ({ children }) => {
                             <button  className='mr-2 py-1 px-2 shadow dark:hover:shadow hover:shadow-lg hover:shadow-emerald-200 hover:bg-emerald-300 dark:hover:text-slate-900 dark:active:text-white active:bg-emerald-500 active:text-white duration-200 hover:rounded hover:border-transparent dark:border-blue-500 dark:hover:border-transparent border-b-2'>Projects</button>
                         </Link>
                         <Link href="/articles">
-                            <button  className='mr-2 py-1 px-2  duration-200 rounded dark:border-transparent border-b-2' disabled>Articles -Coming Soon!</button>
+                            <button  className='mr-2 py-1 px-2 hidden md:inline duration-200 rounded dark:border-transparent border-b-2' disabled>Articles -Coming Soon!</button>
                         </Link>
                     </div>
                     <div className="ml-auto flex items-center gap-2">
                         {theme == 'light' ? 
                         <button onClick={() => setTheme('dark')} className="p-2 bg-slate-700 shadow-lg shadow-slate-400 active:shadow-inner active:bg-slate-500 text-white rounded-2xl duration-200 hover:shadow-slate-300"><BsMoonStars/></button> : 
                         <button onClick={() => setTheme('light')} className="p-2 bg-orange-500 active:shadow-inner active:bg-orange-400 text-white rounded-2xl duration-200 hover:shadow-md hover:shadow-orange-600"><BsSunFill/></button>  }
-                        <button className='p-2 text-white bg-purple-700 shadow-lg dark:shadow-none dark:hover:shadow-lg dark:hover:shadow-purple-600 shadow-purple-400 active:shadow-inner active:bg-purple-500 rounded-2xl duration-200 hover:shadow-purple-200'><BsGithub/></button>
+                        <button onClick={() => window.open('https://github.com/aryabhthakur/')} className='p-2 text-white bg-purple-700 shadow-lg dark:shadow-none dark:hover:shadow-lg dark:hover:shadow-purple-600 shadow-purple-400 active:shadow-inner active:bg-purple-500 rounded-2xl duration-200 hover:shadow-purple-200'><BsGithub/></button>
                     </div>
                 </div>
-                <div className="flex items-center h-[90vh] p-4">
-                    <div className="w-1/2">
+                <div className="flex items-center h-[90vh] flex-col md:flex-row p-4">
+                    <div className="w-full md:w-1/2 order-2 md:order-1 pb-8 md:pb-0">
                         {children}
                     </div>
-                    <div className="w-1/2 flex items-center flex-col">
+                    <div className="w-full md:w-1/2 flex items-center flex-col order-1 md:order-2 mb-8 md:mb-0">
                     {theme == 'light' ? 
-                        <ApplicationLogo className="block h-50 w-full fill-white" /> :
+                        <ApplicationLogo className="block h-28 md:h-full w-full fill-white" /> :
                         <img src="/gg_logo.png" alt="" /> }
                         <div className="mt-2 flex justify-center gap-3">
                             <a href="https://github.com/aryabhthakur" target="_blank" rel="noopener noreferrer"><BsGithub className='inline text-2xl mr-1'/><span className='underline'>aryabhthakur</span></a>
